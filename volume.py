137707e269102e1a645355db67739be87bfe6aa8
sudhanshu55 <sharmasudhanshu2110@gmail.com>
2018-06-02 12:13:18 +0530

    newcode

--- volume.py
+++ volume.py
@@ -8,13 +8,15 @@ def signfind(x):
 
 
 def main():
+
+    avgnoise=subprocess.check_output('soundmeter --collect --seconds 3 | grep avg',shell=True)
+    strbuff=str(avgnoise)
+    print('*********************')
+    noisevolprev=int(strbuff[14:])
+    print(noisevolprev)
+    print('*********************')
     while True:
-        avgnoise=subprocess.check_output('soundmeter --collect --seconds 3 | grep avg',shell=True)
-        strbuff=str(avgnoise)
-        print('*********************')
-        noisevolprev=int(strbuff[14:])
-        print(noisevolprev)
-        print('*********************')
+        
         avgnoise=subprocess.check_output('soundmeter --collect --seconds 3 | grep avg',shell=True)
         strbuff=str(avgnoise)
         noisevolcurr=int(strbuff[14:])
@@ -22,12 +24,13 @@ def main():
         print(noisevolcurr)
         print('*********************')
         update_vol=noisevolcurr-noisevolprev
-
+        noisevolprev=noisevolcurr
         print(update_vol)
 
         update_vol=update_vol/70;
         print(update_vol)
-        os.system("amixer set 'Master' " + str(abs(update_vol)) +"%"+signfind(update_vol))
+        os.system("pactl set-sink-volume 0 " +signfind(update_vol)+ str(abs(update_vol)) +"%")
+        
 
 if __name__ == '__main__':
 	main()
